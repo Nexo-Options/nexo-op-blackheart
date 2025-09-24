461b5ae2c27db16ed61da033e0986f342c23bc24
Nexo Options <nexonexo712@gmail.com>
2025-09-24 19:46:20 +0300

    Create Web3Input.vue

---
+++ components/general/Web3Input.vue
@@ -0,0 +1,76 @@
+<script>
+import { utils } from 'ethers'
+import { toBN } from 'utils/BN'
+
+export default {
+  name: "TokenAmountInput",
+  emits: ['update:modelValue', 'close'],
+  props: {
+    modelValue: {
+      type: Object,
+      default: null
+    },
+    decimals: {
+      type: Number,
+      default: 18
+    },
+    fallback: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    rgx() {
+      const d = parseInt(this.decimals) || 18
+      return new RegExp(`^\\d*\\.?\\d{0,${d}}$`)
+    },
+    value: {
+      get() {
+        if (!this.modelValue) return ''
+        const d = parseInt(this.decimals) || 18
+        const rr = this.modelValue.toString().padStart(d, 0)
+
+        const stripTrailingZeros = x => {
+          let found = false
+          return x
+            .split('')
+            .reverse()
+            .filter(ch => {
+              if (found) return true
+              if (ch === '0') return false
+              found = true
+              return true
+            })
+            .reverse()
+            .join('')
+        }
+
+        const fractional = stripTrailingZeros(rr.substring(rr.length - d))
+        return (rr.substring(0, rr.length - d) || '0') + (fractional ? '.' + fractional : '')
+      },
+      set(val) {
+        const d = parseInt(this.decimals) || 18
+        const value = val === '' ? null : utils.parseUnits('0' + val, d)
+
+        if (
+          (this.modelValue != null) ^ (value != null) ||
+          (this.modelValue != null && value != null && !value.eq(this.modelValue))
+        ) {
+          this.$emit('update:modelValue', value)
+        }
+      }
+    }
+  },
+  methods: {
+    check(e) {
+      if (!this.rgx.test(e.target.value + e.key)) {
+        e.preventDefault()
+      }
+    }
+  }
+}
+</script>
+
+<template lang="pug">
+input(v-model="value" @keypress="check" :placeholder="fallback || '0.0'")
+</template>
