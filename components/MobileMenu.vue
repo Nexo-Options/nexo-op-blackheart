4e35f2b138af78973bd83d94ca4ef31ae1cff49a
Nexo Options <nexonexo712@gmail.com>
2025-09-24 19:56:12 +0300

    Create MobileMenu.vue

---
+++ components/MobileMenu.vue
@@ -0,0 +1,41 @@
+<script>
+import MyTabs from './Tabs.vue'
+
+export default {
+  name: "MobileMenu",
+  emits: ['close'],
+  data() {
+    return {
+      popUp: false,
+      popUpStep: 0,
+      _time: 0,
+      intID: null,
+      state: 'wait'
+    }
+  },
+  components: {
+    MyTabs
+  }
+}
+</script>
+
+<template lang="pug">
+.overlay.overlay-mobile-menu
+  .mobile-menu
+    .close-pop-up.close-pop-up--mobile-menu(@click="$emit('close')")
+    my-tabs(@close-pop-up="$emit('close')")
+
+    .disconnect-info(v-if="$store.getters.account")
+      .hash {{ $store.getters.account }}
+      .disconnect-info__status Connected
+
+    button.button.secondary(
+      v-if="$store.getters.account"
+      @click="$store.dispatch('connection/toggle')"
+    ) Disconnect
+
+    button.button.primary(
+      v-else
+      @click="$store.dispatch('connection/toggle')"
+    ) Connect Wallet
+</template>
