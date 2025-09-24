edb4553a1fa2deb9bd0ceac81f4e2691ea0bbb5d
Nexo Options <nexonexo712@gmail.com>
2025-09-24 19:53:58 +0300

    Create Header.vue

---
+++ components/Header.vue
@@ -0,0 +1,56 @@
+<script>
+import { mapGetters, mapActions } from 'vuex'
+import MyTabs from './Tabs.vue'
+import MobileMenu from './MobileMenu.vue'
+
+export default {
+  name: "AppHeader",
+  data() {
+    return {
+      popUp: false,
+      popUpStep: 0,
+      _time: 0,
+      intID: null,
+      state: 'wait',
+      mobileMenu: false
+    }
+  },
+  computed: {
+    ...mapGetters(['account'])
+  },
+  methods: {
+    ...mapActions({ connect: "connection/toggle" })
+  },
+  components: {
+    MyTabs,
+    MobileMenu
+  }
+}
+</script>
+
+<template lang="pug">
+.section.header
+  .section-content
+    .icon-mobile-menu(
+      v-if="$route.path.split('/')[1] !== ''"
+      @click="mobileMenu = true"
+    )
+    mobile-menu(:class="{ open: mobileMenu }" @close="mobileMenu = false")
+    router-link.logo(to="/")
+    my-tabs
+    .disconnect-info(v-if="account")
+      a.hash(
+        :href="`https://etherscan.io/address/${account}`"
+        target="_blank"
+      ) {{ account }}
+      .disconnect-info__status Connected
+    .header__button(v-if="$route.path.split('/')[1] !== ''")
+      button.button.secondary(
+        v-if="account"
+        @click="connect"
+      ) Disconnect
+      button.button.primary(
+        v-else
+        @click="connect"
+      ) Connect Wallet
+</template>
