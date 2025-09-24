396137fded93ba77fa8cb534bbd6c449721b79c2
Nexo Options <nexonexo712@gmail.com>
2025-09-24 19:57:02 +0300

    Create Tabs.vue

---
+++ components/Tabs.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  name: "AppTabs",
+  emits: ['close-pop-up'],
+  data() {
+    return {
+      tabs: [
+        // Example structure:
+        // { id: "swap-wrap", path: "/swap-wrap/wrap?from=ETH&to=WHETH", title: "Wrap / Swap" },
+        // { id: "holdings", path: "/holdings", title: "Holdings" },
+        // { id: "stake", path: "/stake/stake", title: "Stake" }
+      ]
+    }
+  }
+}
+</script>
+
+<template lang="pug">
+.tabs(v-if="$route.path.split('/')[1] !== ''")
+  router-link.tab(
+    v-for="tab in tabs"
+    :key="tab.id"
+    :to="tab.path"
+    :class="{ selected: $route.path.split('/')[1] === tab.id }"
+    @click="$emit('close-pop-up')"
+  ) {{ tab.title }}
+</template>
