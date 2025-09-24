498350a5997e375b488ae8b780dac3018fffcab0
Nexo Options <nexonexo712@gmail.com>
2025-09-24 19:48:56 +0300

    Create Stake.vue

---
+++ components/staking/Stake.vue
@@ -0,0 +1,73 @@
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: "ClaimHistory",
+  data() {
+    return {
+      popUp: false,
+      popUpStep: 0,
+      _time: 0,
+      intID: null,
+      state: 'wait',
+      selector: [
+        {
+          id: "unstake",
+          title: "Unstake",
+          path: "/stake/unstake"
+        }
+      ]
+    }
+  },
+  computed: {
+    ...mapState({
+      claimHistory: state => state.staking.claimHistory
+    })
+  }
+}
+</script>
+
+<template lang="pug">
+.section
+  .section-content
+    .selector-swap-wrap
+      router-link.selector-swap-wrap__elem(
+        v-for="elem in selector"
+        :key="elem.id"
+        :to="elem.path"
+        :class="{selected: $route.path.split('/')[2] === elem.id}, elem.id"
+      )
+        .selector-swap-wrap__title {{ elem.title }}
+
+    router-view
+
+    .my-table-container(v-if="claimHistory.length > 0")
+      .my-table-container__title History
+      .my-table-box
+        table.my-table
+          tr.my-table__header
+            th Action
+            th Amount
+            th Transaction Hash
+          tr.my-table__line(v-for="claim in claimHistory" :key="claim.transactionHash")
+            td
+              .value.bold.claimed Claimed
+            td
+              .token.bold.USDC
+                | {{ claim.args.amount.format(6, 2) }} USDC
+            td
+              a.hash(:href="`https://etherscan.io/tx/${claim.transactionHash}`" target="_blank")
+                {{ claim.transactionHash }}
+
+      // Mobile version
+      .my-table-box--mobile.text-left
+        .my-table__line(v-for="claim in claimHistory" :key="claim.transactionHash")
+          .token.bold.USDC
+            | {{ claim.args.amount.format(6, 2) }} USDC
+            .action-absolute
+              .value.bold.claimed Claimed
+          .margin-16
+          .my-table-line-elem__title Transaction Hash:
+          a.hash(:href="`https://etherscan.io/tx/${claim.transactionHash}`" target="_blank")
+            {{ claim.transactionHash }}
+</template>
