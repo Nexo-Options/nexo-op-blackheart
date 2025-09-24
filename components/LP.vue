083da6ce066f8006ecf840210ad714d04ca1bb02
Nexo Options <nexonexo712@gmail.com>
2025-09-24 19:55:49 +0300

    Create LP.vue

---
+++ components/LP.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+.section.f1
+  .section-content.main-content
+    .info-box
+      .info-box__text
+        | Nexo contracts by Whiteheart
+        br
+        | help DeFi users to protect value
+        br
+        | of their holdings in ETH and WBTC
+      .info-box__buttons
+        a.button.secondary.icon-external-link(
+          :href="require('assets/Whiteheart_One_Pager.pdf').default"
+          target="_blank"
+        ) Learn More
+        router-link.button.primary.icon-play(to="/swap-wrap/wrap?from=ETH&to=WHETH")
+          | Protect WBTC & ETH Value
+      .info-box__timer-box
+        .timer-box__header
+          span Whiteheart is the Red Cross of crypto.
+          br
+          span It saves your money.
+    .info-box__illustration
+</template>
