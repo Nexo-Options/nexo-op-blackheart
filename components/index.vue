ba2d432d892ac59b54a6eec6fd376434587cd438
Nexo Options <nexonexo712@gmail.com>
2025-09-24 19:57:32 +0300

    Create index.vue

---
+++ components/index.vue
@@ -0,0 +1,63 @@
+<script>
+import { mapState } from 'vuex'
+
+import MyHeader from '../components/Header.vue'
+import MyFooter from '../components/Footer.vue'
+import Notification from '../components/Notification.vue'
+
+export default {
+  name: "MainLayout",
+  data() {
+    return {
+      popUp: false,
+      popUpStep: 0,
+      _time: 0,
+      intID: null,
+      state: 'wait',
+      loading: true
+    }
+  },
+  mounted() {
+    // could be used for app init or polling
+  },
+  beforeUnmount() {
+    clearInterval(this.intID)
+  },
+  computed: {
+    ...mapState(['items']),
+    notificationBind: {
+      get() {
+        return this.$store.state.notifications.bind
+      },
+      set(value) {
+        this.$store.commit('notifications/set', value)
+      }
+    }
+  },
+  methods: {
+    nextStep() {
+      this.popUpStep = (this.popUpStep + 1) % 5
+    },
+    prevStep() {
+      this.popUpStep = (this.popUpStep + 4) % 5
+    },
+    step(n) {
+      this.popUpStep = n
+    }
+  },
+  components: {
+    MyHeader,
+    MyFooter,
+    Notification
+  }
+}
+</script>
+
+<style lang="stylus" src="styles/index.styl"/>
+
+<template lang="pug">
+my-header
+router-view
+my-footer
+notification(v-model="notificationBind")
+</template>
