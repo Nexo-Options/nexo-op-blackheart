23da8b843928da39fe53511bbfbaa8c5e94598b2
Nexo Options <nexonexo712@gmail.com>
2025-09-24 19:50:26 +0300

    Create SwapWrap.vue

---
+++ components/swap-wrap/SwapWrap.vue
@@ -0,0 +1,26 @@
+<script>
+import SelectorSwapWrap from './SelectorSwapWrap.vue'
+
+export default {
+  name: "SwapWrapSection",
+  data() {
+    return {
+      popUp: false,
+      popUpStep: 0,
+      _time: 0,
+      intID: null,
+      state: 'wait'
+    }
+  },
+  components: {
+    SelectorSwapWrap
+  }
+}
+</script>
+
+<template lang="pug">
+.section
+  .section-content
+    selector-swap-wrap
+    router-view
+</template>
