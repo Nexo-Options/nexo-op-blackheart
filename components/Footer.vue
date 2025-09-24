8d7a6f2cb8afe776a8188b1af45a18ab068e67d6
Nexo Options <nexonexo712@gmail.com>
2025-09-24 19:53:39 +0300

    Create Footer.vue

---
+++ components/Footer.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+.section.footer
+  .section-content
+    .footer-box
+      .logo--nexo
+        .logo--nexo__nexo
+          | NEXO
+        .logo--nexo__divider
+        .logo--nexo__info
+          | Whiteheart is built
+          br
+          | on Nexo protocol
+      .copyright
+        | 2025 © ontracts Built on Nexo
+</template>
